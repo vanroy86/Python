d58e9cc057f6179f20f4a8c2ac5d9919a1171edc
IrrelevantPenguin <andrew.darnton12@gmail.com>
2014-04-30 20:43:20 +0100

    Count how many words are in a string

--- count_words.py
+++ count_words.py
@@ -1,12 +1,2 @@
-if __name__ == '__main__':
-	inputstring = raw_input("Enter a sentence to count: \n") # Get input from user
-	
-	counts = [] # Declare empty array
-
-	for i in inputstring:
-		if i.isalnum() or i.isspace(): # if inputstring has a number or a space in it append to counts
-			counts.append(i)
-		else:
-			counts.append(' ')
-	new_input = "".join(counts)
-	print len(new_input.split()) # Print length of counts
+inputString = raw_input("Enter a String: ").split()
+print len(inputString)
