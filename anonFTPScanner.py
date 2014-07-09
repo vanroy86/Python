de90086a6e694af2adc44579ecc5708672760c56
IrrelevantPenguin <andrew.darnton12@gmail.com>
2014-07-09 15:30:27 +0100

    Add commandline parser

--- anonFTPScanner.py
+++ anonFTPScanner.py
@@ -1,13 +1,27 @@
+import optparse
 import ftplib
-def anonLogin(hostname):
+def anonLogin(hostname, email):
 	try:
 		ftp.ftplib.FTP(hostname)
-		ftp.login("anonymous","someone@domain.com")
+		ftp.login("anonymous",email)
 		print "\n[*] " + str(hostname) + " FTP Anonymous Logon Succeeded."
 		ftp.quit()
 		return True
 	except Exception, e:
 		print "\n[-] " + str(hostname) + " FTP Anonymous Logon Failed."
 		return False
-host = "192.168.1.4"
-anonLogin(host)
+def main():
+	parser = optparse.OptionParser("Usage: " + "-H <Target Host> -E <Specify an Email Address>")
+	parser.add_option("-H", dest = "host", type = "string", help = "Specify a Target Host")
+	parser.add_option("-E", dest = "emailAddress", type = "string", help = "Specify an Email Address")
+	(options, args)  = parser.parse_args()
+
+	if (options.host == None or options.emailAddress == None):
+		print parser.usage
+		exit(0)
+	else:
+		host = options.host
+		emailAddress = options.emailAddress
+	anonLogin(host, emailAddress)
+if __name__ == "__main__":
+	main()
