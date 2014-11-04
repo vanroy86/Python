922fd7bc17af30c129f78911c794e4b64ed42209
IrrelevantPenguin <andrew.darnton12@gmail.com>
2014-11-04 21:44:56 +0000

    Update

--- pythonTextEditor.py
+++ pythonTextEditor.py
@@ -2,35 +2,44 @@ import wx
 import os
 class pythonTextEditor(wx.Frame):
     def __init__(self, parent, title):
-        frame = wx.Frame.__init__(self, parent, title = title, size=(600, 400))
+        frame = wx.Frame.__init__(self, parent, title = title, size=(700, 500))
         self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
         self.CreateStatusBar() # A Status bar at the bottom of the window
 
         # Create a menu to insert into the menu bar
         filemenu = wx.Menu()
+        editmenu = wx.Menu()
 
         self.hasOpenedFile = False
         self.currentFileName = ""
         self.isNewFile = True
 
-        menuNew = filemenu.Append(wx.ID_NEW, "New", "Create A New File")
-        menuOpen = filemenu.Append(wx.ID_OPEN, "Open", "Open A File")
-        menuSave = filemenu.Append(wx.ID_SAVE, "Save", "Save A File")
-        menuAbout = filemenu.Append(wx.ID_ABOUT, "About", "Information About This Program")
+        # filemenu config
+        menuNew = filemenu.Append(wx.ID_NEW, "New", "Create new file")
+        menuOpen = filemenu.Append(wx.ID_OPEN, "Open", "Open a file")
+        menuSave = filemenu.Append(wx.ID_SAVE, "Save", "Save a file")
+        menuAbout = filemenu.Append(wx.ID_ABOUT, "About", "Information about this program")
         filemenu.AppendSeparator()
-        menuExit = filemenu.Append(wx.ID_EXIT, "Exit", "Terminate The Program")
+        menuExit = filemenu.Append(wx.ID_EXIT, "Exit", "Terminate the program")
+
+        #editmenu config
+        editPreferences = editmenu.Append(wx.ID_ANY, "Preferences", "Configure the editor")
 
         menuBar = wx.MenuBar()
         menuBar.Append(filemenu, "File") # Populate our menu bar with our menu
+        menuBar.Append(editmenu, "Edit")
         self.SetMenuBar(menuBar)
 
-        # Set up bind events
+        # filemenu Binds
         # More information about events can be found at: wiki.wxpython.org/ListOfEvents
         self.Bind(wx.EVT_MENU, self.on_new, menuNew)
         self.Bind(wx.EVT_MENU, self.on_open, menuOpen)
         self.Bind(wx.EVT_MENU, self.on_save, menuSave)
         self.Bind(wx.EVT_MENU, self.on_about, menuAbout)
         self.Bind(wx.EVT_MENU, self.on_exit, menuExit)
+
+        # editmenu Binds
+        self.Bind(wx.EVT_MENU, self.on_preferences, editPreferences)
 
         self.Show(True)
 
@@ -70,6 +79,11 @@ class pythonTextEditor(wx.Frame):
         self.hasOpenedFile = False
         self.currentFileName = ""
         self.control.SetValue("")
+
+    def on_preferences(self, event):
+        noResizeStyle = wx.DEFAULT_FRAME_STYLE & ~ (wx.RESIZE_BORDER | wx.RESIZE_BOX | wx.MAXIMIZE_BOX)
+        preferencesFrame = wx.Frame(None, -1,title="Preferences", size=(600,800),style=noResizeStyle)
+        preferencesFrame.Show(True)
 
 app = wx.App(False)
 frame = pythonTextEditor(None, "Small editor")
