0ed0d3c023823966875c59d79765a9e973c81a6b
IrrelevantPenguin <andrew.darnton12@gmail.com>
2014-12-09 15:05:19 +0000

    Product Inventory Update

--- projects/Classes/Product Inventory Project /inventory_project.py
+++ projects/Classes/Product Inventory Project /inventory_project.py
@@ -29,26 +29,44 @@ class Inventory(object):
     def update_product(self):
         pass
 
-    def view(self, yaml_data):
-        print yaml.dump(yaml_data, default_flow_style =False)
+    def view_inventory(self, yaml_data):
+        print yaml.dump(yaml_data, default_flow_style = False)
 
+    def get_search_term(self, yaml_data):
+        search_term = raw_input("[+] Type the name of the product you want to search: ")
+        return search_term.lower()
+
+    def search_inventory(self,yaml_data):
+        product_name = self.get_search_term(yaml_data)
+
+        try: # If the product actually exists then print it otherwise throw an error
+            print "\n[+] Product found: " + product_name
+            print yaml.dump(yaml_data["Products"][product_name], default_flow_style = False)
+        except KeyError, e:
+            print "\n[!] Product not found: " + str(e)
+            main()
 def main():
     inventory = Inventory()
 
+    # Open the YAML file and load it into a variable
     yaml_file = open("./products.yaml")
     yaml_data = yaml.safe_load(yaml_file)
 
+    # Set up a loop so the program doesn't end and display the menu
     running = True
-
     while running:
-        print "-- Product Inventory --"
+        print "\n-- Product Inventory --"
         print "-- Options --\n"
         print "1. View inventory"
+        print "2. Search inventory"
+        print "3. Exit"
 
-        user_input = int(raw_input(">"))
+        user_input = int(raw_input("> "))
 
         if user_input == 1:
-            inventory.view(yaml_data)
+            inventory.view_inventory(yaml_data)
+        elif user_input == 2:
+            inventory.search_inventory(yaml_data)
         else:
             running = False
 
